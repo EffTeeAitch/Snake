897464bba17f971ac0a1f06448a9097ffe03b7b3
EffTeeAitch <szymon.jozwiak@poczta.onet.pl>
2022-01-08 15:37:49 +0100

    Upload project
    Add project to implement gitignore

---
+++ Assets/Scripts/DifferentFood.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifferentFood : MonoBehaviour
+{
+    public BoxCollider2D expandedGridArea;
+    public GameObject player;
+    private AudioSource _audio = new AudioSource();
+    public int score = 0;
+
+    private void Start()
+    {
+        RandomizePosition();
+        _audio = GetComponent<AudioSource>();
+    }
+
+    public void RandomizePosition()
+    {
+        Bounds bounds = this.expandedGridArea.bounds;
+
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            RandomizePosition();
+            _audio.PlayOneShot(_audio.clip, 0.2f);
+        }
+        
+        if (other.CompareTag("Obstacle"))
+        {
+            RandomizePosition();
+        }
+
+
+    }
+
+}
---
+++ Assets/Scripts/Placement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using System;
+
+public class Placement : MonoBehaviour
+{
+    private Snake _snakeScript;
+    private bool isFree;
+    public GameObject player;
+    private GameObject[] Walls = new GameObject[4];
+    private CinemachineVirtualCamera cinema;
+
+    private void Start()
+    {
+        _snakeScript = GameObject.Find("Snake").GetComponent<Snake>();
+        cinema = GameObject.Find("Cinemachine").GetComponent<CinemachineVirtualCamera>();
+        for(int i =0; i<Walls.Length; i++)
+        {
+            Walls = (GameObject.FindGameObjectsWithTag("Walls"));
+        }
+
+    }
+
+    private void Update()
+    {
+        EasterWall();
+        CameraMovement();
+    }
+    
+    
+    private void EasterWall()       // function disabling RightWall in case of collecting 20 points
+    {
+        if (_snakeScript._scoreInfo >= 20)
+        {
+            //foreach(GameObject w in Walls)
+            //{
+            //    Destroy(w);
+            //}
+            
+            isFree = true;
+        }
+        else
+        {   
+
+
+            isFree = false;
+        }
+    }
+
+    private void Instantiate(GameObject w, Vector3 vector3)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void CameraMovement()
+    {
+        if (isFree)
+        {
+            //GameObject.Find("MainCamera").GetComponent<CinemachineBrain>().enabled = true;
+            cinema.enabled = true;
+            
+        }
+        else{
+            //GameObject.Find("MainCamera").GetComponent<CinemachineBrain>().enabled = false;
+            cinema.enabled = false;
+            this.transform.position = new Vector3(0, 0, this.transform.position.z);
+        }
+    }
+
+
+
+}
+
